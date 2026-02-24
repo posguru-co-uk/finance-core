95afa5c2f2d7c3946b6475273bb61be1aec8dc56
Devidas <k.devidas@posguru.co.in>
2026-02-24 16:46:22 +0530

    Set departments attribute to override in StaffProfileAttributes

--- src/profile/data-models/staff.ts
+++ src/profile/data-models/staff.ts
@@ -73,7 +73,7 @@ export const StaffProfileAttributes = {
         jsonProperty: "departments",
         format: (value: any) => JSON.parse(value),
         cast: (value: any) => JSON.stringify(value),
-        override: false,
+        override: true,
     } as AttributeConfig<string[]>,
    
 }
